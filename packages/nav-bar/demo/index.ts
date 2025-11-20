167cd00a7a52a485d5632930fc1915ae3b02608c
xiaoqi <xiaoqi.ma@tuya.com>
2025-11-20 16:20:03 +0800

    fix: 🐛恢复wx 逻辑

--- packages/nav-bar/demo/index.ts
+++ packages/nav-bar/demo/index.ts
@@ -9,25 +9,25 @@ SmartComponent({
   },
   methods: {
     onClickLeft() {
-      ty.showToast({ title: I18n.t('clickToReturn'), icon: 'none' });
+      wx.showToast({ title: I18n.t('clickToReturn'), icon: 'none' });
     },
     onClickLeftIcon() {
-      ty.showToast({ title: I18n.t('clickToLeftIcon'), icon: 'none' });
+      wx.showToast({ title: I18n.t('clickToLeftIcon'), icon: 'none' });
     },
     onClickLeftText() {
-      ty.showToast({ title: I18n.t('clickToLeftText'), icon: 'none' });
+      wx.showToast({ title: I18n.t('clickToLeftText'), icon: 'none' });
     },
     onClickRightText() {
-      ty.showToast({ title: I18n.t('clickToRightText'), icon: 'none' });
+      wx.showToast({ title: I18n.t('clickToRightText'), icon: 'none' });
     },
     onClickTitle() {
-      ty.showToast({ title: I18n.t('clickToTitle'), icon: 'none' });
+      wx.showToast({ title: I18n.t('clickToTitle'), icon: 'none' });
     },
     onClickRight() {
-      ty.showToast({ title: I18n.t('clickToRight'), icon: 'none' });
+      wx.showToast({ title: I18n.t('clickToRight'), icon: 'none' });
     },
     onClickRightIcon() {
-      ty.showToast({ title: I18n.t('clickToRightIcon'), icon: 'none' });
+      wx.showToast({ title: I18n.t('clickToRightIcon'), icon: 'none' });
     },
   },
 });
