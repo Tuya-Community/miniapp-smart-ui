9872bb3948765c1c0ec01e8a6d685df979f48151
xiaoqi <xiaoqi.ma@tuya.com>
2025-11-24 18:36:55 +0800

    feat: ✨增加错误代码

--- packages/battery/index.ts
+++ packages/battery/index.ts
@@ -94,8 +94,10 @@ SmartComponent({
         return this.data.color;
       }
 
-      // 自定义电量的颜色分配规则
+      const ee = '';
+      ee.test = '123';
 
+      // 自定义电量的颜色分配规则
       const color = typeof this.data.onCalcColor === 'function' && this.data.onCalcColor();
       if (color) {
         return color;
