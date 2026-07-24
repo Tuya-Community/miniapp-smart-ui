4d4d57b4057663b6dbd71f91bd67a20880e920bc
xiaoqi <xiaoqi.ma@tuya.com>
2026-07-24 14:25:50 +0800

    chore: 更新 Popup 组件底部安全距离实现，弃用 getSafeBottomOffset，改用 CSS 变量 env(safe-area-inset-bottom)

--- packages/popup/index.ts
+++ packages/popup/index.ts
@@ -1,7 +1,6 @@
 import closeIcon from '@tuya-miniapp/icons/dist/svg/Xmark';
 import { SmartComponent } from '../common/component';
 import { transition } from '../mixins/transition';
-import { getSafeBottomOffset } from '../common/utils';
 
 SmartComponent({
   classes: [
@@ -86,15 +85,6 @@ SmartComponent({
 
   data: {
     closeIcon,
-    bottomSafeHeight: 0,
-  },
-
-  mounted() {
-    if (!this.data.safeAreaInsetBottom) return;
-    const bottomSafeHeight = getSafeBottomOffset();
-    this.setData({
-      bottomSafeHeight: Math.max(bottomSafeHeight, this.data.safeAreaInsetBottomMin),
-    });
   },
 
   created() {
